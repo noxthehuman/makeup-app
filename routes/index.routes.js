f42d6a2de100d0737b2b390624f60b2a39a7141e
Nox <benaziz.ines@gmail.com>
2022-03-28 17:08:24 +0200

    search bar added

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -9,4 +9,19 @@ router.get("/private", isLoggedIn, (req, res, next) => {
   res.render("private");
 });
 
+router.search("/", (req, res) => {
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Host': 'makeup.p.rapidapi.com',
+      'X-RapidAPI-Key': '46cbb44bdemsh14887596afca861p12aacfjsn4e39c5a66cde'
+    }
+  };
+  
+  fetch('https://makeup.p.rapidapi.com/products.json', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+})
+
 module.exports = router;
