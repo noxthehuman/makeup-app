76768a9c232391faa2b54fd0b2565d6a5cad8008
Nox <benaziz.ines@gmail.com>
2022-03-29 11:53:53 +0200

    api functionnal

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,46 +1,15 @@
 const router = require("express").Router();
-const isLoggedIn = require("../middleware/isLoggedIn");
-
+const isLoggedIn = require("../middleware/isLoggedIn")
+const {getProducts, getAllProducts} = require("../api/get.products")
 
 /* GET home page */
 router.get("/", (req, res, next) => {
-  res.render("index");
-});
+  res.render("index")
+})
 
-const base_url = 'http://localhost:3000/'
-const apiHost = 'https://makeup.p.rapidapi.com'
-const apiKey = '46cbb44bdemsh14887596afca861p12aacfjsn4e39c5a66cde'
-
-
-router.get("/private", isLoggedIn, (req, res, next) => {
-  res.render("private");
-});
-
-const options = {
-	method: 'GET',
-	headers: {
-		'X-RapidAPI-Host': 'makeup.p.rapidapi.com',
-		'X-RapidAPI-Key': '46cbb44bdemsh14887596afca861p12aacfjsn4e39c5a66cde'
-	}
-};
-
-
-router.get("/", async (req, res) => {
-  const url = `${apiHost}/products.json?${req.body}&appid=${apiKey}`
-  try {
-    fetch('https://makeup.p.rapidapi.com/products.json?brand=colourpop&product_category=lipstick', options)
-	  .then(response => response.json())
-	  .then(response => console.log(response))
-    
-    const response = await fetch(url)
-    const productInfo = await response.json()
-
-
-  }
-  catch(error) {
-    console.error(error)
-  }
-
+router.get('/products', async (req, res, next) => {
+  const products =  await getProducts(req.query)
+  res.render('index', {products})
 })
 
 module.exports = router;
