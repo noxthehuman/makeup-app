989dadf2b9e842700d1b2484755ed531e98dc050
lb2718 <98560887+lb2718@users.noreply.github.com>
2022-03-30 12:15:53 +0200

    route added for product details

--- routes/index.routes.js
+++ routes/index.routes.js
@@ -1,13 +1,15 @@
-const router = require('express').Router()
-const isLoggedIn = require('../middleware/isLoggedIn')
-const Product = require('../models/Product.model')
+const router = require("express").Router();
+const isLoggedIn = require("../middleware/isLoggedIn");
+const Product = require("../models/Product.model");
 
 /* GET home page */
-router.get('/', (req, res, next) => {
+router.get("/", (req, res, next) => {
   //console.log(req.session, 'i am in products and this is req session')
-  res.render('index')
-})
+  res.render("index");
+});
 
+router.get("/product-detail", (req, res, next) => {
+  res.render("../views/prod-detail.hbs");
+});
 
-
-module.exports = router
+module.exports = router;
