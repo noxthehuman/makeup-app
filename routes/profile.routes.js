421543cf7d34fbf7186a8a14dcdcf4a90f97c755
lb2718 <98560887+lb2718@users.noreply.github.com>
2022-03-31 15:29:09 +0200

    css in progress

--- routes/profile.routes.js
+++ routes/profile.routes.js
@@ -1,22 +1,30 @@
 const router = require("express").Router();
 const isLoggedin = require("../middleware/isLoggedIn");
-const Favorite = require('../models/Favorites.model');
+const Favorite = require("../models/Favorites.model");
 const Look = require("../models/Looks.model");
 
 /* GET profile page */
-router.get('/', isLoggedin, async (req, res, next) => {
+router.get("/", isLoggedin, async (req, res, next) => {
   try {
     const userFavorite = await Favorite.find({
-      user: req.session.user._id
-    }).populate("product")
+      user: req.session.user._id,
+    }).populate("product");
 
-    const looks = await Look.find({createdBy: req.user._id}).populate(this.all)
-    
-    res.render('profile', { user: req.user, userFavorite, looks })
-  } 
-  catch (error) {
-    console.error(error)
+    const looks = await Look.find({ createdBy: req.user._id }).populate(
+      "mascara",
+      "eyeliner",
+      "eyeshadow",
+      "eyebrows",
+      "bronzer",
+      "blush",
+      "foundation",
+      "lipstick"
+    );
+
+    res.render("profile", { user: req.user, userFavorite, looks });
+  } catch (error) {
+    console.error(error);
   }
-})
+});
 
 module.exports = router;
